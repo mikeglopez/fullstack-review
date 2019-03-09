84ed5d7f9e921664cf73ce90d067162709c8dde1
Mike <mikeglopez.91@gmail.com>
2019-03-09 00:03:20 -0600

    Complete save function for database

--- database/index.js
+++ database/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher', { useMongoClient: true });
 
 let repoSchema = mongoose.Schema({
-  id: Number,
+  id: {type: Number, unique: true},
   name: String,
   url: String,
   stargazers_count: Number,
@@ -12,10 +12,22 @@ let repoSchema = mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (repoList) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
+    for (var repo = 0; repo < repoList.length; repo++) {
+      var record = new Repo(repoList[repo]);
+      record.save()
+        .then(repo => {
+          console.log(repo)
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    }
+    console.log('repo.find', Repo.find());
 }
 
 module.exports.save = save;
